9fa0d8c6e93fa4cf58f866ec2156512169eeb6a9
Cole van Krieken <colevankrieken@gmail.com>
2018-02-21 15:19:03 -0800

    Allele probabilities

---
+++ stronghold_007_iprb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+'''
+Given: Three positive integers k, m, and n, representing a population
+containing k+m+n organisms: k individuals are homozygous dominant for a factor,
+m are heterozygous, and n are homozygous recessive.
+
+Return: The probability that two randomly selected mating organisms will
+produce an individual possessing a dominant allele (and thus displaying the
+dominant phenotype). Assume that any two organisms can mate.
+'''
+
+from rosalind import *
+
+def dominant_allele_probability(k, m, n):
+    total = k + m + n
+    return (
+        k / total +
+        m / total * 0.5 +
+        m / total * 0.5 * k / (total - 1) +
+        m / total * 0.5 * (m - 1) / (total - 1) * 0.5 +
+        n / total * k / (total - 1) +
+        n / total * m / (total - 1) * 0.5
+    )
+
+
+if __name__ == '__main__':
+    k, m, n = map(int, rosalind_input().strip().split())
+    print(dominant_allele_probability(k, m, n))
